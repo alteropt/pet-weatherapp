778318fe0a9d598f4375d4d138b284aa17410b56
alteropt <vladqwer035@gmail.com>
2025-12-07 22:38:12 +0300

    fix sidebar bug

--- src/hooks/useOutsideClicker.js
+++ src/hooks/useOutsideClicker.js
@@ -18,9 +18,11 @@ export const useOutsideClicker = (
 		}
 
 		window.addEventListener('click', handleClickOutside)
+		window.addEventListener('touchstart', handleClickOutside)
 
 		return () => {
 			window.removeEventListener('click', handleClickOutside)
+			window.removeEventListener('touchstart', handleClickOutside)
 		}
 	}, [parentRef, setIsParentOpen, triggerWidth])
 }
--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,8 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+	base: '/pet-weatherapp/',
+	build: {
+		outDir: 'dist',
+	},
 })
